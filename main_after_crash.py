49c98fcf7e7a8e885d1b75318fcfeb7a98cb4401
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-04-13 13:26:44 +0200

    add main after a crash with ic

--- main_after_crash.py
+++ main_after_crash.py
@@ -24,12 +24,13 @@ import Grain
 import User
 import Report
 import main
+import Owntools.Compute
 
 #-------------------------------------------------------------------------------
 #User
 #-------------------------------------------------------------------------------
 
-name_to_load = 'Dicts/save_tempo'
+name_to_load = 'Dicts/save_ic'
 
 #-------------------------------------------------------------------------------
 #load data
@@ -43,7 +44,10 @@ dict_geometry = dict_save['geometry']
 dict_material = dict_save['material']
 dict_sample = dict_save['sample']
 dict_sollicitation = dict_save['sollicitation']
-dict_tracker = dict_save['tracker']
+if name_to_load == 'Dicts/save_tempo':
+    dict_tracker = dict_save['tracker']
+if name_to_load == 'Dicts/save_ic':
+    dict_ic = dict_save['ic']
 simulation_report = dict_save['report']
 
 #-------------------------------------------------------------------------------
@@ -56,7 +60,23 @@ simulation_report.write('\nA crash occurs...\n\n')
 #main
 #-------------------------------------------------------------------------------
 
+if name_to_load == 'Dicts/save_ic' :
+
+    #trackers
+    Owntools.Compute.Compute_mass(dict_sample)
+    Owntools.Compute.Compute_compacity(dict_sample)
+    Owntools.Compute.Compute_k0_ic(dict_ic, dict_sample)
+    dict_tracker = {
+    'L_mass' : [dict_sample['grains_mass']],
+    'L_mass_dissolved' : [0],
+    'L_z_box_max' : [dict_sample['z_box_max']],
+    'L_compacity' : [dict_sample['compacity']],
+    'L_k0' : [dict_sample['k0']]
+    }
+
 main.main_simulation(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitation, dict_tracker, simulation_report)
+close_main(dict_algorithm, dict_geometry, dict_material, dict_sample, dict_sollicitation, dict_tracker, simulation_report)
+
 
 #-------------------------------------------------------------------------------
 #close simulation
